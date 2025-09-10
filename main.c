8b8db778c12e94b28e27cefec3517e3abf85f78c
abdelilah ouslimane <abdelilah.ouslimane3740>
2025-09-10 17:04:27 +0100

    initialise de projet

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct{
+    int idAvion;
+    char modele[30];
+    int capacite;
+    int statut;
+}Avions;
+
+typedef struct{
+    char nom[20];
+    int nbAvions;
+    Avions avion[200];
+}aeroport;
+void affichage_aeroport(){
+    
+}
+int main(){
+
+}
